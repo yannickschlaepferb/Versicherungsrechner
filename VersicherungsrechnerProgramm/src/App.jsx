f34a940695278b802784e73592b616157b416320
yannickschlaepferb <yannick.schlaepfer@lernende.bbw.ch>
2023-04-14 15:05:57 +0200

    did reset & berechnen functions

--- VersicherungsrechnerProgramm/src/App.jsx
+++ VersicherungsrechnerProgramm/src/App.jsx
@@ -1,37 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [vw, setVW] = useState(1)
+  const [vw, setVW] = useState("");
+  const [vs, setVS] = useState("");
+  const [schaden, setSchaden] = useState("");
+  const [result, setResult] = useState("");
+
+  function berechnen() {
+    if(vs != "" && vw != "" && schaden != "") {
+    setResult(() => {
+      if (Math.round((vs / vw) * schaden * 100) / 100 >= schaden) {
+        return `ihre Versicherung übernimmt ${schaden} CHF`;
+      } else {
+        return `Ihre Versicherung übernimmt ${
+          Math.round((vs / vw) * schaden * 100) / 100
+        } CHF`;
+      }
+    });
+  }
+  else {
+    alert("Sie müssen alle Felder ausfüllen!")
+  }
+  }
+
+  function reset() {
+    setVW("   ");
+    setVS("");
+    setSchaden("");
+    setResult("");
+  }
+
+  function überOderUnterVersicherung() {
+    
+  }
+
   return (
     <div className="App">
-
-      <h1>Schadensrechner/Home  About Me</h1> 
+      <h1>Schadensrechner/Home About Me</h1>
       <div className="ClientAngaben">
+        
+        <label for="Versicherungssumme">
+          Bitte geben Sie Ihre Versicherungssumme an:
+        </label>
+        <input
+          type="number"
+          id="Versicherungssumme"
+          name="Versicherungssumme"
+          min="1"
+          max="100000000"
+          value={vs}
+          onChange={(e) => setVS(e.target.value)}
+        />
 
-      <label for="Versicherungswert">Bitte geben Sie Ihren Versicherungswert an:</label>
-      <input type="number" id="Versicherungswert" name="Versicherungswert" min="1" max="100000000" value={vw} onChange={(e) => setVW(e.target.value)}/>
+        <label for="Versicherungswert">
+          Bitte geben Sie Ihren Versicherungswert an:
+        </label>
+        <input
+          type="number"
+          id="Versicherungswert"
+          name="Versicherungswert"
+          min="1"
+          max="100000000"
+          value={vw}
+          onChange={(e) => setVW(e.target.value)}
+        />
 
-      <label for="Versicherungswert">Bitte geben Sie Ihre Versicherungssumme an:</label>
-      <input type="number" id="Versicherungssumme" name="Versicherungssumme" min="1" max="100000000"/>
-
-      <label for="Versicherungswert">Bitte geben Sie den angerichteten Schaden an:</label>
-      <input type="number" id="Schaden" name="Schaden" min="1" max="100000000"/>
-
+        <label for="Schaden">
+          Bitte geben Sie den angerichteten Schaden an:
+        </label>
+        <input
+          type="number"
+          id="Schaden"
+          name="Schaden"
+          min="1"
+          max="100000000"
+          value={schaden}
+          onChange={(e) => setSchaden(e.target.value)}
+        />
       </div>
       <div className="Ergebnis">
-
-      <label for="zuBezahlenderSchaden">Zu bezahlender Schaden:</label>
-      <p>
-        {vw}
-      </p>
-
-      <label id="Tipp" >Sie sollten sich mehr/weniger versichern</label>
-
-      <button></button>
-
+        <button id="Berechnen" onClick={berechnen}>
+          Berechnen
+        </button>
+        <p>{result}</p>
+        <button id="reset" onClick={reset}>
+          Reset
+        </button>
       </div>
+      <p></p>
     </div>
   );
 }
