2ef44ca1797882771f6210d1c5dd1bea74824c9a
yannickschlaepferb <yannick.schlaepfer@lernende.bbw.ch>
2023-04-14 11:27:37 +0200

    erste Komponenten

--- VersicherungsrechnerProgramm/src/App.jsx
+++ VersicherungsrechnerProgramm/src/App.jsx
@@ -1,16 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [vw, setVW] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <label for="homeLabel">Home:</label>
-        <input type="text" name="home" id="home" />
 
-        <label for="guestLabel">Guest:</label>
-        <input type="text" name="guest" id="guest"/>
-      </header>
+      <h1>Schadensrechner/Home  About Me</h1> 
+      <div className="ClientAngaben">
+
+      <label for="Versicherungswert">Bitte geben Sie Ihren Versicherungswert an:</label>
+      <input type="number" id="Versicherungswert" name="Versicherungswert" min="1" max="100000000" value={vw} onChange={(e) => setVW(e.target.value)}/>
+
+      <label for="Versicherungswert">Bitte geben Sie Ihre Versicherungssumme an:</label>
+      <input type="number" id="Versicherungssumme" name="Versicherungssumme" min="1" max="100000000"/>
+
+      <label for="Versicherungswert">Bitte geben Sie den angerichteten Schaden an:</label>
+      <input type="number" id="Schaden" name="Schaden" min="1" max="100000000"/>
+
+      </div>
+      <div className="Ergebnis">
+
+      <label for="zuBezahlenderSchaden">Zu bezahlender Schaden:</label>
+      <p>
+        {vw}
+      </p>
+
+      <label id="Tipp" >Sie sollten sich mehr/weniger versichern</label>
+
+      <button></button>
+
+      </div>
     </div>
   );
 }
